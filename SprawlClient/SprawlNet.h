bcce4c86ae4facae07463dea5592b00a544648f3
Ole Andre Birkedal <birkedal85@Ole-Andre-Birkedals-MacBook-Air.local>
2011-05-29 16:40:50 +0200

    Enkel connectionsak

--- SprawlClient/SprawlNet.h
+++ SprawlClient/SprawlNet.h
@@ -3,14 +3,28 @@
 //  SprawlClient
 //
 //  Created by Ole Andre Birkedal on 5/29/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Kosepeisen. All rights reserved.
 //
+
+#pragma once
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <errno.h>
+#include <iostream>
 
 class SprawlNet
 {
 public:
-    SprawlNet();
+    SprawlNet( const std::string &Host, const std::string &Port );
     ~SprawlNet();
+    
+    size_t SendData( const void *Data, size_t Length ) const;
+    bool IsConnected() { return Connected; }
+
 private:
-    // ...
+    int Socket;
+    bool Connected;
+    int Connect( const std::string &host, const std::string &port );
 };
--- SprawlClient/main.cpp
+++ SprawlClient/main.cpp
@@ -3,7 +3,7 @@
 //  SprawlClient
 //
 //  Created by Ole Andre Birkedal on 5/29/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Kosepeisen. All rights reserved.
 //
 
 #include <iostream>
@@ -12,7 +12,14 @@
 
 int main (int argc, const char * argv[])
 {
-
+    SprawlNet *Network = new SprawlNet( "127.0.0.1", "1337");
+    
+    std::string test("lalal1337");
+    
+    if( Network->IsConnected() )
+        Network->SendData( test.c_str(), test.length() );
+    
+    delete Network;
 
     return 0;
 }
