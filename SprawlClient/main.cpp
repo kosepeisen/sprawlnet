e7d3775c5529b88894b10a9038a5ebcdc87010c5
Ole Andre Birkedal <birkedal85@Ole-Andre-Birkedals-MacBook-Air.local>
2011-05-29 14:58:34 +0200

    Initial Commit

---
+++ SprawlClient/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SprawlClient
+//
+//  Created by Ole Andre Birkedal on 5/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
