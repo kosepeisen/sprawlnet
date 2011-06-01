36b6569cfeefadc2243e2c75b5389a56c1ccdccd
Kjetil Ørbekk <kjetil.orbekk@gmail.com>
2011-06-01 07:37:22 +0200

    Send messages in Sprawlnet format.
    Prefix the message with the number of bytes in the message, encoded as a
    string.

--- client/SprawlClient/main.cpp
+++ client/SprawlClient/main.cpp
@@ -6,18 +6,38 @@
 //  Copyright 2011 Kosepeisen. All rights reserved.
 //
 
+#include <string.h>
+
 #include <iostream>
 
 #include "SprawlNet.h"
+
+void CreateMessage(std::string Content, char **MessageDest, size_t *LengthDest) {
+    *MessageDest = new char[Content.length() + sizeof(int)];
+    *LengthDest = Content.length();
+    memcpy(*MessageDest, LengthDest, sizeof(int));
+    memcpy(*MessageDest + sizeof(int), Content.c_str(), Content.length());
+}
+
+void SendMessage(const SprawlNet *Network, std::string Message) {
+    char *MessageBuffer;
+    size_t Length;
+
+    CreateMessage(Message, &MessageBuffer, &Length);
+    Network->SendData( MessageBuffer, Length );
+    delete MessageBuffer;
+}
 
 int main (int argc, const char * argv[])
 {
     SprawlNet *Network = new SprawlNet( "127.0.0.1", "1337");
     
-    std::string test("lalal1337");
-    
-    if( Network->IsConnected() )
-        Network->SendData( test.c_str(), test.length() );
+    if( Network->IsConnected() ) {
+        SendMessage(Network, "lalalal1337");
+        SendMessage(Network, "SprawlNet loading...");
+        SendMessage(Network, "SprawlNet loading....");
+        SendMessage(Network, "SprawlNet loading.....");
+    }
     
     delete Network;
 
